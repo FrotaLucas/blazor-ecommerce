a4b3bd02cf9cb3a485e36d7ed75eca039ca20236
FrotaLucas <lucasdiasfrota@gmail.com>
2024-04-01 14:43:00 +0200

    import projetct share within project client

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace BlazorEcommerce.Shared
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Price { get; set; } = string.Empty;
+
+
     }
 }
