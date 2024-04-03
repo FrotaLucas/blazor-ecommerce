d16e25e72d942a8d829eef0697386be2ff84edcf
FrotaLucas <lucasdiasfrota@gmail.com>
2024-04-03 20:59:35 +0200

    create service module for the client project

---
+++ BlazorEcommerce/Client/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorEcommerce.Client.Services
+{
+    public interface IProductService
+    {
+        //pq essa propriedade aqui dentro da interface ????? Ele esta sendo preenchido
+        //atraves do metodo GetProduct no arquivo ProductService. Mas como isso ?
+        List<Product> Products {get; set;}
+
+        //pq essse Task nao tem List ou ServiceResponse com encapsulamento ??????????
+        Task GetProduct();
+    }
+}
---
+++ BlazorEcommerce/Client/Services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorEcommerce.Client.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _http;
+        public ProductService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Product> Products { get; set; } =  new List<Product>();
+
+        public async Task GetProduct() 
+        { 
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+
+            Products = result.Data;
+        }
+    }
+}
